0a37bd9b79997c718343f38d56f67031c7b59fb7
distilledMilk <atharva.velani@gmail.com>
2025-05-30 08:22:48 +0800

    read

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringVal, times) {
+    if (times < 0){
+        return "ERROR";
+    }
+    finalString = "";
+    for (let i = 0; i < times; i++) {
+        
+        finalString = finalString + stringVal;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
