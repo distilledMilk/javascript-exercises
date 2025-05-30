4bcf5019bb0e151be502ed74fd7ad002a38bf7ff
distilledMilk <atharva.velani@gmail.com>
2025-05-30 10:56:26 +0800

    reverse with inbuilt functions OR FOR LOOPS

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
 
+    if(inputString.length === 0) return "";
+
+    if(inputString.length === 1) return inputString;
+    
+    return inputString.split('').reverse().join('');
 };
 
 // Do not edit below this line
