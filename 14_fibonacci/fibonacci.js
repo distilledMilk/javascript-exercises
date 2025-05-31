b2d95877ad60479bb1aca4e8358a7bad20a09046
distilledMilk <atharva.velani@gmail.com>
2025-05-31 14:07:54 +0800

    fib lol

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index);
+    var fibA = 0;
+    var fibB = 1;
+    var fibC = 0;
+
+    
+    // console.log(index);
+    if (index < 0 ) return "OOPS";
+    if(index === 1) return 1;
+    for(let i = 1; i < index; i ++){
+        // console.log("index: " + i);
+        // console.log("FibC: " + fibC + " FibB: " + fibB + " FibA: " + fibA);
+        fibC = fibA + fibB;
+       
+
+
+        fibA = fibB;
+        fibB = fibC;
+        // console.log("AFTER");
+        // console.log("FibC: " + fibC + " FibB: " + fibB + " FibA: " + fibA);
+
+    }
+    return fibC;
 
 };
 
