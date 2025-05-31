0cd234a206c031cd3ce9a2c942bff101fadb07d2
distilledMilk <atharva.velani@gmail.com>
2025-05-31 13:03:10 +0800

    number, mathround and tofixed)

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32 ) * (5/9);
+
+  return celsius = Number((Math.round(celsius * 10)/10).toFixed(1));
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  fahrenheit = (celsius* (9/5)) + 32;
+
+  return fahrenheit = Number((Math.round(fahrenheit * 10)/10).toFixed(1));
+
 };
 
 // Do not edit below this line
