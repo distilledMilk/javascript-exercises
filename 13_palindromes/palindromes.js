3804cacee94475ff49c62d08c50355d49d98e8b9
distilledMilk <atharva.velani@gmail.com>
2025-05-31 13:41:24 +0800

    dont forget the whitespace

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (isPalindrome) {
 
+    var punctuation = /[\.,?! ]/g;
+    var confirmed;
+    isPalindrome = isPalindrome.replace(punctuation, "").toLowerCase();
+    // isPalindrome = isPalindrome.replace(" ", "").toLowerCase();
+    var reversed = isPalindrome.split('').reverse().join('');
+    console.log(isPalindrome);
+
+    isPalindrome === reversed ? confirmed = true : confirmed = false;
+
+    return confirmed;
 };
 
 // Do not edit below this line
