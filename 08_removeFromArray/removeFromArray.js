b2a7be668ec2726520b97ba2c3666a09bdd79178
distilledMilk <atharva.velani@gmail.com>
2025-05-31 10:48:21 +0800

    decrement

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    numOfArgs = arguments.length - 1;
+
+    var count = 1;
+    for(let element of elements){
+        for(let i=0; i< arr.length; i++){
+            if(arr[i] === element){
+                arr.splice(i, 1);
+                i--;
+                console.log(count);
+                console.log("index:" + i);
+                count++;
+            }
+           
+        }
+    }
+
+    // for (let index = 0; index < arr.length; index++) {
+    //     for(let element of elements){
+    //         if(arr[index] === element) arr.splice(index, 1);
+    //     }
+        
+    // }
+    return arr;
 };
 
 // Do not edit below this line
