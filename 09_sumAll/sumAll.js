99db5a841d7c219896bbf3ce4898215eca4db4af
distilledMilk <atharva.velani@gmail.com>
2025-05-31 11:06:04 +0800

    isInteger lol

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if((Number.isInteger(a) && Math.sign(a) === 1) && (Number.isInteger(b) && Math.sign(b) === 1)){
+        sum = 0;
+        a > b ? lowest = b  : lowest = a;
+        a > b ? highest = a : highest = b;
+
+        for(i = lowest; i < highest+1; i++){
+            sum = sum + i;
+        }
+        return sum;
+    }   
+    return "ERROR";
 };
 
 // Do not edit below this line
