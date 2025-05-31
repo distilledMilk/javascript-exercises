20a4ad851d833e24efdb9d94dfca5286f0802d94
distilledMilk <atharva.velani@gmail.com>
2025-05-31 14:12:06 +0800

    simple getters

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    var arr = [];
+    for(let i = 0; i < books.length; i++){
 
+        arr.push(books[i].title);
+    }
+    return arr;
 };
 
 // Do not edit below this line
